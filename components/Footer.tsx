cc71d74f5458d019186e04f1e73c6e93737d755c
José Isaac <joseisaacnascimento@gmail.com>
2026-01-04 20:32:05 -0300

    feat(footer): criar footer

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer className="w-full flex items-center justify-between p-4">
+      <p>NoteLog</p>
+      <p>
+        Desenvolvido por{" "}
+        <a
+          href="https://github.com/joseisaacpy"
+          target="_blank"
+          className="underline"
+        >
+          José Isaac
+        </a>
+      </p>
+    </footer>
+  );
+}
