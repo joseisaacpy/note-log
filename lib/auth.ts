52bbaa3353163e03daa80d992fd749220244a6d3
José Isaac <joseisaacnascimento@gmail.com>
2026-01-04 15:54:08 -0300

    fix(auth): corrigir provedor para github

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,14 @@
 import NextAuth from "next-auth";
 import { prisma } from "@/lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import Resend from "next-auth/providers/resend";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Resend({
-
-  })],
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
 });
