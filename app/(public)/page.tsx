68b6fc2aa46e55dbbb369de9fa39440c81839f8b
José Isaac <joseisaacnascimento@gmail.com>
2026-01-07 22:35:27 -0300

    feat(home): transformar página em server page para pegar informações do banco - adicionar parágrafo com quantidade de usuários vindo do banco

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/prisma";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Page() {
+export default async function Page() {
+  const qtdUsers = await prisma.user.findMany();
   return (
     <section className="section min-h-[80vh] flex flex-col-reverse items-center justify-center gap-8">
-      <div className="space-y-4 text-center">
+      <div className="space-y-6 text-center">
         <h1 className="text-3xl md:text-4xl font-bold max-w-2xl text-center">
           Conheça o <span className="text-primary">NoteLog</span>, seu diário
           pessoal com autenticação!
@@ -14,6 +16,11 @@ export default function Page() {
         <Link href="/api/auth/signin">
           <Button size="lg">Entrar com GitHub</Button>
         </Link>
+
+        <p className="mt-2">
+          Já somos {qtdUsers.length} pessoas que usam o NoteLog! Voce também
+          pode se juntar!
+        </p>
       </div>
 
       <Image
